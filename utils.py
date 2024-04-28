f41adb12a9cfab958ec5626f82e0376334d13abf
das91t70 <161017685+das91t70@users.noreply.github.com>
2024-04-28 12:17:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -41,6 +41,19 @@ def imshow(img, labels):
     plt.title(labels)
     plt.show()
 
+# plot loss and accuracy graphs
+def plot_loss_and_accuracy_graphs(train_loss, train_acc, test_loss, test_acc):
+    t = [t_items.item() for t_items in train_loss]
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(t)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_loss)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
+
 def find_max_lr_rangetest(test_type, model, train_loader, val_loader=None):
   if (test_type == "fastai"):
     criterion = nn.CrossEntropyLoss()
